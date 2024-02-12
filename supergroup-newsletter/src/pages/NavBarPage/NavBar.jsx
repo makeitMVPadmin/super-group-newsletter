4f228fe4a33d0a17166091df22af92004602035f
Mikikahana <kmichoel25@gmail.com>
2024-02-12 14:36:11 -0500

    Add icons

--- supergroup-newsletter/src/pages/NavBarPage/NavBar.jsx
+++ supergroup-newsletter/src/pages/NavBarPage/NavBar.jsx
@@ -8,8 +8,13 @@ const Navbar = () => {
     setClickedIndex(index === clickedIndex ? null : index);
   };
 
-  // Define an array of nav items
-  const navItems = ["Home", "Communities", "Events", "Chats"];
+  // Define an array of nav items with their corresponding icons
+  const navItems = [
+    { name: "Home", iconClass: "icon1" },
+    { name: "Communities", iconClass: "icon2" },
+    { name: "Events", iconClass: "icon3" },
+    { name: "Chats", iconClass: "icon4" },
+  ];
 
   return (
     <div className="navbar">
@@ -22,12 +27,20 @@ const Navbar = () => {
       {navItems.map((item, index) => (
         <div
           key={index}
-          className={`nav-item home${clickedIndex === index ? " clicked" : ""}`}
+          className={`nav-item names${clickedIndex === index ? " clicked" : ""}`}
           onClick={() => handleClick(index)}
         >
-          <span>{item}</span>
+          <div className="nav-item-content">
+            <span className={`${item.iconClass} icon`}></span> {/* Render the icon using the CSS class */}
+            <span>{item.name}</span> {/* Render the item text */}
+          </div>
         </div>
       ))}
+      <div className="nav-item-content">
+        <span className="icon icon5"></span>
+        <span className="icon icon6"></span>
+        <span className="icon icon7"></span>
+      </div>
     </div>
   );
 };
