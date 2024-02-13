c233406b97bd11ba26edfa6e93172f4143775f74
Sheena Sang <sheenasany@gmail.com>
2024-02-12 22:24:00 -0500

    moved AI input box to the bottom of landing page

--- supergroup-newsletter/src/pages/LandingPage/LandingPage.jsx
+++ supergroup-newsletter/src/pages/LandingPage/LandingPage.jsx
@@ -6,11 +6,11 @@ import './LandingPage.css'
 export default function LandingPage() {
   return (
     <div>
-      {/* <AiInputBox /> */}
       <ConfirmPopup 
         title='You’ve Scheduled a Newsletter!'
         paragraph='The Communiti will be receiving their copy of the newsletter shortly.'
       />
+      <AiInputBox />
     </div>
   )
 }
