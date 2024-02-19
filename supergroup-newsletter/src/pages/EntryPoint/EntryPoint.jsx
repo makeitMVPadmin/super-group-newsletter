fe6acfff35335bb07e3c42c12a92b0b35829314d
Sheena Sang <sheenasany@gmail.com>
2024-02-18 21:03:38 -0500

    middle column complete, except for a submit button on the ai input box

--- supergroup-newsletter/src/pages/EntryPoint/EntryPoint.jsx
+++ supergroup-newsletter/src/pages/EntryPoint/EntryPoint.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import { ReactComponent as BackBtn } from "../../assets/svgs/back-btn.svg"
 import { ReactComponent as UploadPhoto } from "../../assets/svgs/upload-photo.svg"
-import "./EntryPoint.css";
+import { ReactComponent as ToggleOff} from "../../assets/svgs/toggle-off.svg"
+import { ReactComponent as ToggleOn} from "../../assets/svgs/toggle-on.svg"
+import {ReactComponent as AIicon} from "../../assets/svgs/ai-icon.svg";
 import CheckboxHeaders from "../../components/CheckboxHeaders/CheckboxHeaders";
+import "./EntryPoint.css";
 
 export default function EntryPoint() {
     const [isClicked, setIsClicked] = useState(false);
+    const [isToggled, setIsToggled] = useState(false);
+    const [userInput, setUserInput] = useState("");
 
+    //handling back button, goes nowhere for now, but show move user to Communiti's page (maybe a dashboard)
     const handleBackClick = () => {
-        setIsClicked(true)
+        setIsClicked(!isClicked);
         console.log("clicked!")
     }
 
+    // handling the toggle, just for show for now, will add implementation later
+    const toggleHandler = () => {
+        setIsToggled(!isToggled)
+    }
+
+    // rendering Checkbox component for Events and sending dummy data 
     const renderEvents = () => {
-        
         return events.map((event, index) => (
             <CheckboxHeaders
                 key={index}
@@ -22,7 +33,7 @@ export default function EntryPoint() {
             />
         )) 
     }
-
+    // rendering the Checkbox component for Announcements and sending dummy data
     const renderAnnouncements = () => {
         //
         return announcements.map((announcement, index) => (
@@ -34,6 +45,26 @@ export default function EntryPoint() {
         ))
     }
 
+    // mapping over dummy data and creating a div to display members when toggled
+    const renderMembers = () => {
+        return members.map((member, index) => (
+        <div className="members" key={index} index={member.uuid}>
+            {member.member}
+        </div>
+
+        )) 
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Textarea content: ', userInput)
+    }
+
+    const handleUserInput = (e) => {
+        setUserInput(e.target.value)
+    }
+
+    // Dummy data 
     const events = [
         {uuid: 1, description: "Come enjoy fireworks at the shire!"},
         {uuid: 2, description: "Learn how to avoid the Nazgul 101."},
@@ -44,6 +75,12 @@ export default function EntryPoint() {
         {uuid: 1, description: "Fellowship embarks on perilous quest."},
         {uuid: 2, description: "Ring destroyed, peace restored."},
         {uuid: 3, description: "Return of the King crowns victory."}
+    ]
+
+    const members = [
+        {uuid: 1, member: "Frodo"},
+        {uuid: 2, member: "Gandalf"},
+        {uuid: 3, member: "Aragorn"},
     ]
 
 
@@ -74,11 +111,40 @@ export default function EntryPoint() {
                             {renderAnnouncements()}
                         </div>
                     </div>
+
                     <div className="middle-column">
-                        
+                        <h4 className="member-title">Show Members</h4>
+                        <h5 className="member-subheader">Toggle to include the three most recently joined members.</h5>
+                        <div className="toggle-container">
+                            <input
+                                type="checkbox"
+                                id="toggle"
+                                className="hidden-checkbox"
+                                checked={isToggled}
+                                onChange={toggleHandler}
+                            />
+                            <label htmlFor="toggle" className="toggle-label">
+                                {isToggled ? <ToggleOn /> : <ToggleOff />}
+                            </label>
+                        </div>
+                        <div className="show-members">
+                            {isToggled ? renderMembers() : null}
+                        </div>
+                        <div className="ai-container">
+                            <h4 className="ai-title">Additional Instructions:</h4>
+                            <h5 className="ai-subheader">
+                                <AIicon className="ai-icon" />
+                                Give AI specific instructions for your newsletter.
+                            </h5>
+                            <form onSubmit={handleSubmit} className="ai-input">
+                                <textarea className="ai-textarea" value={userInput} onChange={handleUserInput} placeholder="Example: Hi! Can you mention that Valentine's Day is coming up? You don't need to include the events that I've selected. Can you also share that there will be a fun giveaway coming up in two weeks and to stay tuned?"></textarea>
+                                {/* <button type="submit">Generate Newsletter</button> */}
+                            </form>
+                        </div>
                     </div>
+
                     <div className="right-column">
-                    
+                        
                     </div>
                 </div>
             </div>
