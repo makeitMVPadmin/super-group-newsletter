ddfc7c2831e81cc448c59d64986e4a1df94b44e9
John Rooney <rooneyjohn@gmail.com>
2024-02-19 22:54:54 +0000

    speeling errrors

--- supergroup-newsletter/src/components/NewsFooter/NewsFooter.jsx
+++ supergroup-newsletter/src/components/NewsFooter/NewsFooter.jsx
@@ -10,7 +10,7 @@ export default function NewsFooter() {
   return (
     <div className='footer-container'>
       <img src={mvpLogo} className='footer-mvpLogo'/>
-      <p className='footer-helping'>Helping You Turn Your New Skills into a Succesfful Career.</p>
+      <p className='footer-helping'>Helping You Turn Your New Skills into a Succesful Career.</p>
       <img src={mvpMountain} className='footer-image'/>
       <div className='footer-ending'>
         <div>          
