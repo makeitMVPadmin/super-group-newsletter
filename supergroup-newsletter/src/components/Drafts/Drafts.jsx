55b32d2138d44d96609c696ae736ba02984bbf5e
John Rooney <rooneyjohn@gmail.com>
2024-02-22 20:34:01 +0000

    changed draft to date

--- supergroup-newsletter/src/components/Drafts/Drafts.jsx
+++ supergroup-newsletter/src/components/Drafts/Drafts.jsx
@@ -1,17 +1,24 @@
+import React from 'react';
 import "./Drafts.css";
 import { useApiContext } from '../../components/ApiContext/ApiContext';
 import { useNavigate } from "react-router-dom"; 
 
 export default function Drafts({draft}) {
+  
   const navigate = useNavigate(); 
   // access to the global variables
   const { 
     handleNewsDraftPopulateNewsEditor
   } = useApiContext();
-
+  
+  function formatCreatedDate(draft) {
+    const options = { year: 'numeric', month: 'short', day: '2-digit' };
+    const formattedDate = new Date(draft.createdOn.seconds * 1000).toLocaleDateString('en-US', options);
+    
+    return formattedDate;
+  }
 
   const handleEditClicked = async () => {
-    
     try {
       await handleNewsDraftPopulateNewsEditor(draft.id); // Wait until handleNewsDraftPopulateNewsEditor is finished
       navigate('/newsEditor'); // Once handleNewsDraftPopulateNewsEditor is finished, perform navigation
@@ -19,11 +26,12 @@ export default function Drafts({draft}) {
       console.error(error); // Handle any errors
     }
   }
-    return(
-        <div index={draft.id} className="draft-item-container">    
-            <h5 className="item-title">{draft.title}</h5>
-            <h6 className="item-subheader">{draft.createdBy}</h6>
-            <button className='item-button' onClick={handleEditClicked}>Edit</button>
-        </div>
-    )
+  
+  return (
+    <div index={draft.id} className="draft-item-container">    
+        <h5 className="item-title">{draft.title}</h5>
+        <h6 className="item-subheader">Saved on: {formatCreatedDate(draft)}</h6>
+        <button className='item-button' onClick={handleEditClicked}>Edit</button>
+    </div>
+  );
 }
